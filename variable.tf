8f61c2971c7645d0187b569699fd06b5582d77bd
devops721 <126851749+devops721@users.noreply.github.com>
2023-03-03 12:20:15 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,9 +3,11 @@ variable "vpc_cidr" {
 }
 
 variable "public_cidrs" {
+  public_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
   type    = list(any)
 }
 
 variable "private_cidrs" {
+  private_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
   type    = list(any)
-}
+} 
